b47179fbd7326a547298812f6ac03df101908c4a
WBTOfficial <66641498+WBTOfficial@users.noreply.github.com>
2020-06-09 19:10:08 +0800

    Update .travis.deploy.bash

--- .travis.deploy.bash
+++ .travis.deploy.bash
@@ -13,7 +13,7 @@ else
 	exit 1
 fi
 
-env GO111MODULE=on make ontology-${PLATFORM} tools-${PLATFORM}
+env GO111MODULE=on make WBT-Blockchain-${PLATFORM} tools-${PLATFORM}
 mkdir tool-${PLATFORM}
 cp ./tools/abi/* tool-${PLATFORM} 
 cp ./tools/sigsvr* tool-${PLATFORM} 
@@ -22,6 +22,6 @@ zip -q -r tool-${PLATFORM}.zip tool-${PLATFORM};
 rm -r tool-${PLATFORM};
 
 set +x
-echo "ontology-${PLATFORM}-amd64 |" $(md5sum ontology-${PLATFORM}-amd64|cut -d ' ' -f1)
+echo "WBT-Blockchain-${PLATFORM}-amd64 |" $(md5sum WBT-Blockchain-${PLATFORM}-amd64|cut -d ' ' -f1)
 echo "tool-${PLATFORM}.zip |" $(md5sum tool-${PLATFORM}.zip|cut -d ' ' -f1)
 
